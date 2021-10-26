8ce6850111ec8b755b1ae6007d40bb68cd20ea24
Ricardo Morato <rmr@cin.ufpe.br>
2021-10-26 19:36:13 -0300

    fix: email normalization

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -17,7 +17,7 @@ class ModelTests(TestCase):
 
     def test_new_user_email_normalized(self):
         """Test the email for a new user is normalized"""
-        email = "RICARDOMORATODEV@GMAIL.COM"
+        email = "ricardomoratodev@GMAIL.COM"
         user = get_user_model().objects.create_user(
             email=email,
             password='Test_Pass123'
