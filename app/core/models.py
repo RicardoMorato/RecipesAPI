56df4c7529a3639a1cafb9ec2c749ade4556ff70
Ricardo Morato <rmr@cin.ufpe.br>
2021-10-26 00:09:41 -0300

    fix: flake8 problems

--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                         PermissionsMixin
 
+
 class UserManager(BaseUserManager):
     def create_user(self, email: str, password: str = None, **extra_fields):
         """Creates, saves and returns a new user"""
@@ -22,6 +23,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         """Test creating a new user with email is successful"""
@@ -17,7 +18,10 @@ class ModelTests(TestCase):
     def test_new_user_email_normalized(self):
         """Test the email for a new user is normalized"""
         email = "RICARDOMORATODEV@GMAIL.COM"
-        user = get_user_model().objects.create_user(email=email, password='Test_Pass123')
+        user = get_user_model().objects.create_user(
+            email=email,
+            password='Test_Pass123'
+        )
 
         self.assertEqual(user.email, email.lower())
 
