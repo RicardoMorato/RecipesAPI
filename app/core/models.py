8221b4eaa843dc66dfdd1ed660f18a1b849d8229
Ricardo Morato <rmr@cin.ufpe.br>
2021-10-26 00:00:28 -0300

    feat[user-model]: normalize email upon user creation

--- app/core/models.py
+++ app/core/models.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                         PermissionsMixin
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password = None, **extra_fields):
+    def create_user(self, email: str, password: str = None, **extra_fields):
         """Creates, saves and returns a new user"""
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=email.lower(), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
